c595f48d55b062cde347d22ccd90ed236d63f43b
Túlio Dantas <tuliopragana@hotmail.com>
2024-09-04 21:02:18 -0300

    remoção de erros

--- src/ProdutosLoja/Loja.java
+++ src/ProdutosLoja/Loja.java
@@ -25,25 +25,7 @@ public class Loja {
         camisaRegata.adicionarVariantes("M", 11);
         camisaRegata.adicionarVariantes("G", 32);
         camisaRegata.adicionarVariantes("GG", 22);
-        
 
-
-        roupas.add(new Roupa("Camiseta UV", 149.90, ,"M", "Preto"));
-        roupas.add(new Roupa("Camisa Regata", 74.90, 55 ,"G", "Branco"));
-        roupas.add(new Roupa("Camiseta manga curta", 179.90, 23,"P", "Cinza"));
-        roupas.add(new Roupa("Calção poliamida", 124.90, 18 ,"M", "Preto"));
-        roupas.add(new Roupa("Bermuda Jeans X", 120.00, 44 ,"M", "Bege"));
-        roupas.add(new Roupa("Bermuda Jeans Y", 128.90, 11 ,"G", "Preto"));
-        roupas.add(new Roupa("Calça Legging", 100.00, 80 ,"PP", "Preto"));
-        roupas.add(new Roupa("Top Feminino", 89.90, 8 ,"P", "Vermelho"));
-
-
-        acessorios.add(new Acessorios("Bandana" ,55.30, 100, "Algodão" , "Esportivo"));
-        acessorios.add(new Acessorios("Óculos de ciclísmo" ,200.25, 39, "Policarbonato" , "Esportivo"));
-        acessorios.add(new Acessorios("Relógio tecnológico" ,500.00, 55, "Silicone" , "Esportivo/Tecnologia"));
-        acessorios.add(new Acessorios("Meias cano curto" ,35.00, 111, "Algodão/Poliamida" , "Casual/Esportivo"));
-        acessorios.add(new Acessorios("Meias cano médio" ,40.00, 100, "Algodão/Poliamida" , "Casual/Esportivo"));
-        acessorios.add(new Acessorios("Viseira" ,22.30, 77, "Poliéster" , "Casual/Esportivo"));
 
 
     }
